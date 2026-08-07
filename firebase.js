92c501fb4c7e293e0c92311403534326fc64df53
rynixtech <riteshkumarachar0@gmail.com>
2026-08-07 11:46:27 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4fRxucKX7nWJKuwdT5RX7UFogvDsIXAo",
+  authDomain: "rynixtech-e0281.firebaseapp.com",
+  projectId: "rynixtech-e0281",
+  storageBucket: "rynixtech-e0281.firebasestorage.app",
+  messagingSenderId: "50627783379",
+  appId: "1:50627783379:web:6041bcdf91e1bcbbfb0226",
+  measurementId: "G-6DVJWYB6ML"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
